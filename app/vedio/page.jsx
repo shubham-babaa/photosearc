b867c08e076947d28de816237fd12a6a77862f87
SHU-BABA <manshipatel.deep@gmail.com>
2023-06-04 04:16:49 +0530

    v3

--- app/vedio/page.jsx
+++ app/vedio/page.jsx
@@ -2,19 +2,24 @@
 import React, { useState, useEffect } from 'react';
 import Vediofetch from '../component/VedioFetch';
 import Image from 'next/image';
-import { IoCaretUpCircleOutline,IoCloseSharp} from "react-icons/io5";
+import { IoCaretUpCircleOutline, IoCloseSharp } from "react-icons/io5";
 export default function Page() {
   const [searchQuery, setSearchQuery] = useState();
   const [dta, setDta] = useState('');
   const [showImage, setShowImage] = useState(false);
   const [selectedVideo, setSelectedVideo] = useState(null); // Track selected video
-  const[searchData,setSearchData]=useState('');
+  const [searchData, setSearchData] = useState('');
+  const [k, setK] = useState("https://player.vimeo.com/external/339400446.hd.mp4?s=ac5022d70a4585bb4e6015d52ad614f665724fd9&profile_id=175&oauth2_token_id=57447761")
   useEffect(() => {
     const fetchData = async () => {
-      const data = await Vediofetch(searchQuery?searchQuery:"all");
+      const data = await Vediofetch(searchQuery ? searchQuery : "all");
       if (data.videos.length !== 0) {
         setDta(data.videos);
-        console.log(dta);
+        console.log(dta)
+
+        if (dta.length > 0)
+          setK(dta[0].video_files[0].link)
+        console.log(k);
       }
     };
     fetchData();
@@ -23,6 +28,10 @@ export default function Page() {
 
   const handleSearch = (e) => {
     setSearchQuery(searchData);
+    const k = dta[0].video_files[0].link
+
+    setSearchData('')
+
   };
 
   const handlePlayVideo = (video) => {
@@ -37,29 +46,48 @@ export default function Page() {
 
   useEffect(() => {
     console.log(selectedVideo, showImage);
-  }, [selectedVideo, showImage]);
+  }, [selectedVideo, showImage, k]);
 
   const handleParentClick = (event) => {
     event.stopPropagation(); // Prevent event propagation to the parent div
   };
 
   return (
-    <div className="bg-gray-100 py-8 min-h-screen ">
+    <div className="flex min-h-screen flex-col items-center justify-between  bg-gradient-to-r to-cyan-900 from-gray-900   mx-1 overflow-x-hidden">
       {/* Updated background color to gray */}
-      <div className="flex items-center justify-center mt-10 mb-10">
-        <input
-          type="text"
-          value={searchQuery}
-          onChange={(e)=>{setSearchData(e.target.value)}}
-          className="border border-gray-300 rounded-md py-2 px-4 w-64 sm:w-80 md:w-96"
-          placeholder="Search..."
-        />
-        <button
-          onClick={handleSearch}
-          className="ml-2 py-2 px-4 bg-blue-500 hover:bg-blue-600 text-white rounded-md"
-        >
-          Search
-        </button>
+      <div className="flex  justify-center items-center  mb-20">
+
+        <div className=' text-center'>
+          <video
+            id="videoPlayer"
+            width="100%"
+            height="auto"
+            controls
+            autoplay
+            loop
+            className="w-full h-[600px] "
+          >
+            <source src=
+              "https://player.vimeo.com/external/221214111.hd.mp4?s=02857e21d87a8cd1c0ceeb77ba164d2648b49871&profile_id=170&oauth2_token_id=57447761"
+              type="video/mp4" />
+          </video>
+        </div >
+
+        <div className='absolute sm:top-[80%] sm:left-[35%] top-[80%] flex z-50'>
+          <input
+            type="text"
+            value={searchData}
+            onChange={(e) => { setSearchData(e.target.value) }}
+            className="border border-gray-300 rounded-md py-2 px-4 w-64 sm:w-80 md:w-96"
+            placeholder="Search..."
+          />
+          <button
+            onClick={handleSearch}
+            className="ml-2 py-2 px-4 bg-cyan-500 shadow-lg shadow-cyan-500/50 text-white rounded-md"
+          >
+            Search
+          </button>
+        </div>
       </div>
       <div className="max-w-5xl mx-auto px-2" onClick={handleParentClick}>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
@@ -70,19 +98,19 @@ export default function Page() {
                 <div key={id} className="relative">
                   <div className="rounded overflow-hidden shadow-lg shadow-cyan-500/50">
                     <div
-                      className="w-full h-full bg-black rounded-lg cursor-pointer relative border-4 shadow-lg shadow-cyan-500/50 border-red-900 px-4"
-                       // Call handlePlayVideo when video is clicked
+                      className="w-full h-full bg-blackbg-cyan-500 shadow-3xl shadow-cyan-500 rounded-lg cursor-pointer relative border-4  border-red-900 px-4"
+                    // Call handlePlayVideo when video is clicked
                     >
-                     
+
                       <video
                         width={width}
                         height={height}
-                        className={`w-[500px] h-[500px]  ${showImage ? 'hidden' : ''}`}
+                        className={`w-full h-full ${showImage ? 'hidden' : ''}`}
                       >
                         <source src={link} type={file_type} />
                         Your browser does not support the video tag.
                       </video>
-                      <IoCaretUpCircleOutline className='text-[50px] text-white absolute top-[50%] left-[40%]' onClick={() => handlePlayVideo(obj)}/>
+                      <IoCaretUpCircleOutline className='text-[50px] text-white absolute top-[50%] left-[40%]' onClick={() => handlePlayVideo(obj)} />
                     </div>
                   </div>
                 </div>
@@ -93,12 +121,12 @@ export default function Page() {
           )}
         </div>
         {showImage && selectedVideo && (
-          <div className="fixed top-0 left-0 flex items-center justify-center w-full h-full bg-cyan-600 z-50">
+          <div className="fixed top-0 left-0 flex items-center justify-center w-full h-full bg-gray-900 z-50">
             <button
               className="absolute top-4 right-4 bg-cyan-500  shadow-lg shadow-cyan-500/50 text-white py-2 px-6 ml-2 rounded-lg" // Updated button color to black for light mode
               onClick={handleCloseVideo}
             >
-              <IoCloseSharp/>
+              <IoCloseSharp />
             </button>
             <div className="relative w-3/4 h-3/4">
               <video
